7e1f805c4709dd527d8234c25798a94aab2ba7ba
a.nesic <razor9514@gmail.com>
2019-05-16 00:11:02 +0200

    Lesson3

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,39 @@
 package exercises;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
+public static void main(String[] args) {
+	Random random = new Random();
+	int num = random.nextInt(20);
+	int NoT=0;
+	Scanner scanner = new Scanner(System.in);
+	int Guess;
+	boolean win = false;
+	System.out.println("Guess the number between 1 and 20");
+	while (win ==false) {
+		
+		Guess = scanner.nextInt();
+		NoT++;
+		
+		if (Guess == num) {win = true;
+			}
+		else if (Guess > num) {System.out.println("Try for a lower number..");}
+		else if (Guess < num) {System.out.println("Try for a higher number..");}
+			
+		}
+	System.out.println("You win");
+		}
+	}
+	
+	
+	
+	
+	
+	
 
-}
+
